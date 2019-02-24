75e0f8d842f9c60f35d595ffbd0d479aa93ffa88
sthobis <s.thobi.sinaga@gmail.com>
2019-02-24 14:10:12 +0700

    refactor: rename constants

--- src/index.js
+++ src/index.js
@@ -13,48 +13,45 @@ const gm = new GameManager(io, pm);
 
 const EVENT = {
   CONNECT: "connect",
-  CONNECT_ERROR: "connect_error",
   DISCONNECT: "disconnect",
-  DISCONNECTING: "disconnecting",
-  GENERAL_ERROR: "general_error",
-  CLIENT_JOIN_ROOM: "client_join_room",
-  CLIENT_LEAVE_ROOM: "client_leave_room",
-  CLIENT_SUBMIT_ANSWER: "client_submit_answer",
-  CLIENT_UPDATE_DRAWING: "client_update_drawing",
-  SERVER_JOIN_ERROR: "server_join_error",
-  SERVER_UPDATE_PLAYER_LIST: "server_update_player_list",
-  SERVER_NEW_ANSWER: "server_new_answer"
+  JOIN_ROOM: "join_room",
+  JOIN_ROOM_ERROR: "join_room_error",
+  LEAVE_ROOM: "leave_room",
+  SUBMIT_ANSWER: "submit_answer",
+  UPDATE_DRAWING: "update_drawing",
+  UPDATE_PLAYER_LIST: "update_player_list",
+  UPDATE_ANSWER_LIST: "update_answer_list"
 };
 
 io.on(EVENT.CONNECT, socket => {
-  socket.on(EVENT.CLIENT_JOIN_ROOM, player => {
+  socket.on(EVENT.JOIN_ROOM, player => {
     try {
       pm.addPlayer({ ...player, socketId: socket.id });
-      io.emit(EVENT.SERVER_UPDATE_PLAYER_LIST, pm.players);
+      io.emit(EVENT.UPDATE_PLAYER_LIST, pm.players);
     } catch (err) {
-      socket.emit(EVENT.SERVER_JOIN_ERROR, err);
+      socket.emit(EVENT.JOIN_ROOM_ERROR, err);
       socket.disconnect(true);
     }
   });
 
-  socket.on(EVENT.CLIENT_LEAVE_ROOM, player => {
+  socket.on(EVENT.LEAVE_ROOM, player => {
     pm.removePlayerBySocketId(player.socketId);
-    socket.broadcast.emit(EVENT.SERVER_UPDATE_PLAYER_LIST, pm.players);
+    socket.broadcast.emit(EVENT.UPDATE_PLAYER_LIST, pm.players);
   });
 
-  socket.on(EVENT.CLIENT_SUBMIT_ANSWER, payload => {
+  socket.on(EVENT.SUBMIT_ANSWER, payload => {
     gm.challenge(payload);
-    socket.broadcast.emit(EVENT.SERVER_NEW_ANSWER, payload);
+    socket.broadcast.emit(EVENT.UPDATE_ANSWER_LIST, payload);
   });
 
-  socket.on(EVENT.CLIENT_UPDATE_DRAWING, payload => {
-    socket.broadcast.emit(EVENT.CLIENT_UPDATE_DRAWING, payload);
+  socket.on(EVENT.UPDATE_DRAWING, payload => {
+    socket.broadcast.emit(EVENT.UPDATE_DRAWING, payload);
   });
 
   socket.on(EVENT.DISCONNECT, e => {
     const player = pm.removePlayerBySocketId(socket.id);
     if (player) {
-      socket.broadcast.emit(EVENT.SERVER_UPDATE_PLAYER_LIST, pm.players);
+      socket.broadcast.emit(EVENT.UPDATE_PLAYER_LIST, pm.players);
     }
   });
 });
